9606f7b5039ae09502b3c55ee42cae79d3771790
ecjvaca-crm-brocker <ecjvaca@gmail.com>
2026-09-15 13:17:45 -0500

    Update JP_Brocker.py

--- JP_Brocker.py
+++ JP_Brocker.py
@@ -18,7 +18,7 @@ import streamlit as st
 # ==========================================
 st.set_page_config(
     page_title="ESCALA Consultoría Financiera y Empresarial",
-    page_layout="wide",
+    layout="wide",  # ✅ Parámetro correcto
     initial_sidebar_state="expanded",
 )
 
